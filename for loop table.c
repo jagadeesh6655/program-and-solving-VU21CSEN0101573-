bd90924de547e3512571571ab7c375ef20070729
jagadeesh6655 <93900679+jagadeesh6655@users.noreply.github.com>
2021-12-06 12:25:33 +0530

    Add files via upload

---
+++ for loop table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+	int a,n;
+	printf("enter an integer");
+	scanf("%d",&n);
+	for (a=1; a<=30;++a)
+ 
+ printf("%d* %d = %d \n",n,a,n*a);
+
+  return 0;
+}
+
